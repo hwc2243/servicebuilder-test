2b771fd18d8aa93c045a0dcf165436082cb17594
H. William Connors II <hwconnors@gmail.com>
2024-05-10 16:15:23 -0400

    Add some entity tests

---
+++ src/main/java/com/test/model/BasicBar.java
@@ -0,0 +1,10 @@
+package com.test.model;
+
+import jakarta.persistence.Entity;
+
+import com.test.base.model.BaseBasicBar;
+
+@Entity
+public class BasicBar extends BaseBasicBar
+{
+}
---
+++ src/main/java/com/test/model/SubBasicFoo.java
@@ -0,0 +1,15 @@
+package com.test.model;
+
+public class SubBasicFoo extends BasicFoo {
+	protected String c;
+	
+	public String getC ()
+	{
+		return this.c;
+	}
+	
+	public void setC (String c)
+	{
+		this.c = c;
+	}
+}
